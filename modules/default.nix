fbb848b382259607d7c3188b00cbaf44a2611212
resursator <resursator@gmail.com>
2024-08-10 15:59:35 +0300

    moved nvidia to separate module

--- modules/default.nix
+++ modules/default.nix
@@ -1,16 +1,22 @@
 { config, lib, pkgs, inputs, options, ... }:
 {
+  nixpkgs.config.allowUnfree = true;
+
   home-manager = {
     extraSpecialArgs = { inherit inputs; };
     users."resursator" = import ./nixos/home.nix;
   };
+
   imports = [
+    ./nixos/nvidia.nix
     ./modulebundle.nix
     inputs.home-manager.nixosModules.default
   ];
+
   environment.sessionVariables = {
     FLAKE = "/home/resursator/nixosFlake";
   };
+
   environment.systemPackages = [
     (pkgs.writeTextDir "share/sddm/themes/breeze/theme.conf.user" ''
       [General]
