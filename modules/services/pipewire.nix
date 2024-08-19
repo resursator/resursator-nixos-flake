83f758636f7baef3470406cf66cb1348105886cf
resursator <resursator@gmail.com>
2024-08-19 17:38:17 +0300

    more pipewire & pulseadio tests

--- modules/services/pipewire.nix
+++ modules/services/pipewire.nix
@@ -7,9 +7,27 @@
       context.properties = {
         default.clock.rate = 48000;
         default.clock.allowed-rates = [ 48000 ];
-        default.clock.quantum = 1024;
-        default.clock.min-quantum = 1024;
-        default.clock.max-quantum = 8096;
+        default.clock.quantum = 32;
+        default.clock.min-quantum = 32;
+        default.clock.max-quantum = 32;
+      };
+    };
+    extraConfig.pipewire-pulse."92-low-latency" = {
+      context.modules = [
+        {
+          name = "libpipewire-module-protocol-pulse";
+          args = {
+            pulse.min.req = "32/48000";
+            pulse.default.req = "32/48000";
+            pulse.max.req = "32/48000";
+            pulse.min.quantum = "32/48000";
+            pulse.max.quantum = "32/48000";
+          };
+        }
+      ];
+      stream.properties = {
+        node.latency = "32/48000";
+        resample.quality = 1;
       };
     };
   };
