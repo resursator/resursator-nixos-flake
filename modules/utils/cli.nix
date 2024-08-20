fde36fde8dd561bb1d480b854088a448c538e152
resursator <resursator@gmail.com>
2024-08-20 20:30:57 +0300

    rar added

--- modules/utils/cli.nix
+++ modules/utils/cli.nix
@@ -1,11 +1,12 @@
 { pkgs, ... }:
 {
   environment.systemPackages = with pkgs; [
-    wget
     nh
-    testdisk-qt
-    p7zip
     nil         # LSP for Kate editor
     nixd        # LSP for zed
+    p7zip
+    rar
+    testdisk-qt
+    wget
   ];
 }
