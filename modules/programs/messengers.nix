7329898e4ff9ced61009f46d035fba2c5592c1b7
resursator <resursator@gmail.com>
2024-09-12 15:24:46 +0300

    upgrade & vesktop & pipewire test

--- modules/programs/messengers.nix
+++ modules/programs/messengers.nix
@@ -1,7 +1,7 @@
 { pkgs, ... }:
 {
   environment.systemPackages = with pkgs; [
-    discord
+    vesktop
     telegram-desktop
   ];
 }
--- modules/services/pipewire.nix
+++ modules/services/pipewire.nix
@@ -8,31 +8,13 @@
     services.pipewire = {
       enable = true;
       pulse.enable = true;
-      extraConfig.pipewire."92-low-latency" = {
+      extraConfig.pipewire."92-crackling" = {
         context.properties = {
           default.clock.rate = 48000;
           default.clock.allowed-rates = [ 48000 ];
-          default.clock.quantum = 32;
-          default.clock.min-quantum = 32;
-          default.clock.max-quantum = 32;
-        };
-      };
-      extraConfig.pipewire-pulse."92-low-latency" = {
-        context.modules = [
-          {
-            name = "libpipewire-module-protocol-pulse";
-            args = {
-              pulse.min.req = "32/48000";
-              pulse.default.req = "32/48000";
-              pulse.max.req = "32/48000";
-              pulse.min.quantum = "32/48000";
-              pulse.max.quantum = "32/48000";
-            };
-          }
-        ];
-        stream.properties = {
-          node.latency = "32/48000";
-          resample.quality = 1;
+          default.clock.quantum = 1024;
+          default.clock.min-quantum = 1024;
+          default.clock.max-quantum = 2048;
         };
       };
     };
