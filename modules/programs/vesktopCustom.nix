e910ec1b4dcd473a1aedad214f484c7dc2908162
resursator <resursator@gmail.com>
2025-01-31 19:17:45 +0300

    custom icon for vesktop, it works

--- modules/programs/vesktopCustom.nix
+++ modules/programs/vesktopCustom.nix
@@ -4,14 +4,33 @@
   pkgs,
   ...
 }:
+let
+  customVesktopIcon = pkgs.runCommand "custom-vesktop-icon" {} ''
+    mkdir -p $out
+    cp ${../../resources/discord.png} $out/icon.png
+  '';
+
+  vesktopCustom = pkgs.vesktop.overrideAttrs (oldAttrs: {
+    postInstall = oldAttrs.postInstall or "" + ''
+      rm -r $out/share/icons/hicolor
+      mkdir -p $out/share/icons/hicolor/256x256/apps/
+
+      # Ensure the icon directory exists
+      iconPath="$out/share/icons/hicolor/256x256/apps/vesktop.png"
+
+      # Copy the custom icon (replace with your actual icon file)
+      cp ${customVesktopIcon}/icon.png $iconPath
+    '';
+  });
+in
 {
   options = {
     vesktopCustom.enable = lib.mkEnableOption "enables vesktop with custom icon";
   };
 
   config = lib.mkIf config.vesktopCustom.enable {
-    environment.systemPackages = with pkgs; [
-      vesktop
+    environment.systemPackages = [
+      vesktopCustom
     ];
   };
 }
