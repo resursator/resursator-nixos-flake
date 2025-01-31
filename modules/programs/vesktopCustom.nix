544b370ad2dc3bff7b93ffb53c4889741db6d677
resursator <resursator@gmail.com>
2025-02-01 01:02:50 +0300

    replaced vencord icon & loading gif and added vlc to gui module

--- modules/programs/vesktopCustom.nix
+++ modules/programs/vesktopCustom.nix
@@ -8,10 +8,19 @@ let
   customVesktopIcon = pkgs.runCommand "custom-vesktop-icon" {} ''
     mkdir -p $out
     cp ${../../resources/discord.png} $out/icon.png
+    cp ${../../resources/discord-tray-icon.png} $out/tray-icon.png
+    cp ${../../resources/discord-loading.gif} $out/loading.gif
   '';
 
   vesktopCustom = pkgs.vesktop.overrideAttrs (oldAttrs: {
+    preBuild = oldAttrs.preBuild or "" + ''
+      echo "Replacing tray icon and loading animation"
+      cp ${customVesktopIcon}/tray-icon.png build/icon.png
+      cp ${customVesktopIcon}/tray-icon.png static/icon.png
+      cp ${customVesktopIcon}/loading.gif static/shiggy.gif
+    '';
     postInstall = oldAttrs.postInstall or "" + ''
+      echo "Replacing app icon"
       rm -r $out/share/icons/hicolor
       mkdir -p $out/share/icons/hicolor/256x256/apps/
 
