8ae79e49341f206c2de3de590b406a3e899f5438
resursator <resursator@gmail.com>
2025-02-17 17:26:24 +0300

    removed service

--- modules/programs/rustdesk.nix
+++ modules/programs/rustdesk.nix
@@ -13,21 +13,5 @@
     environment.systemPackages = with pkgs; [
       rustdesk-flutter
     ];
-    systemd.services.rustdesk = {
-      enable = true;
-      description = "rustdesk";
-      serviceConfig = {
-        ExecStart = "${pkgs.rustdesk-flutter}/bin/rustdesk --service";
-        ExecStop = "pkill -f \"rustdesk --\"";
-        PIDFile = "/run/rustdesk.pid";
-        KillMode = "mixed";
-        TimeoutStopSec = "30";
-        User = "root";
-        LimitNOFILE = "100000";
-      };
-      wantedBy = [ "multi-user.target" ];
-      requires = ["network-online.target"];
-      after = ["display-manager.service"];
-    };
   };
 }
