45f4b26ed4482bb8c30114b305216e8a864ffbbd
resursator <resursator@gmail.com>
2025-02-17 16:42:39 +0300

    trying to setup rustdesk service, broken

--- modules/programs/rustdesk.nix
+++ modules/programs/rustdesk.nix
@@ -11,7 +11,23 @@
 
   config = lib.mkIf config.rustdesk.enable {
     environment.systemPackages = with pkgs; [
-        rustdesk
-      ];
+      rustdesk
+    ];
+    systemd.services.rustdesk = {
+      enable = true;
+      description = "rustdesk";
+      serviceConfig = {
+        ExecStart = "${pkgs.rustdesk}/bin/rustdesk --service";
+        ExecStop = "pkill -f \"rustdesk --\"";
+        PIDFile = "/run/rustdesk.pid";
+        KillMode = "mixed";
+        TimeoutStopSec = "30";
+        User = "root";
+        LimitNOFILE = "100000";
+      };
+      wantedBy = [ "multi-user.target" ];
+      requires = ["network-online.target"];
+      after = ["display-manager.service"];
+    };
   };
 }
