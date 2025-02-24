378012eca7cba6188067bf27928cf7baa45faf5d
resursator <resursator@gmail.com>
2025-02-24 15:33:13 +0300

    upgrade & zed old version, since 0.174.5 can't build

--- flake.nix
+++ flake.nix
@@ -4,6 +4,7 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     gimpnixpkgs.url = "github:jtojnar/nixpkgs?ref=gimp-meson";
+    zedoldnixpkgs.url = "github:nixos/nixpkgs?rev=a2e485e7c40448382a4f87ecb2c5aab5593cc782";
     zen-browser = {
       url = "github:0xc000022070/zen-browser-flake";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -23,10 +24,11 @@
   let
     gimppkgs = inputs.gimpnixpkgs.legacyPackages.x86_64-linux;
     zenpkgs = inputs.zen-browser.packages.x86_64-linux;
+    zedpkgs = inputs.zedoldnixpkgs.legacyPackages.x86_64-linux;
   in
   {
     nixosConfigurations.homenix = nixpkgs.lib.nixosSystem {
-      specialArgs = { inherit inputs; inherit gimppkgs; inherit zenpkgs; };
+      specialArgs = { inherit inputs; inherit gimppkgs; inherit zenpkgs; inherit zedpkgs; };
       modules = [
         ./hosts/homenix/configuration.nix
         ./modules/default.nix
