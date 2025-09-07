67c68aba067bccef5214a09b8a29ce9caa22cec5
resursator <resursator@gmail.com>
2025-09-08 02:03:46 +0300

    Revert "telegram wrapper to remove spam in jctl"
    This reverts commit f609a920ecc08644e5e9e99db2c242bddd2a77de.

--- modules/programs/messengers.nix
+++ modules/programs/messengers.nix
@@ -4,30 +4,14 @@
   pkgs,
   ...
 }:
-let
-  telegramWrapper =
-    pkgs.runCommand "telegram-desktop-wrapper"
-      {
-        buildInputs = [ pkgs.telegram-desktop ];
-      }
-      ''
-        mkdir -p $out/bin
-        cat > $out/bin/telegram <<'EOF'
-        #!${pkgs.stdenv.shell}
-        export QT_LOGGING_RULES="qt.gui.imageio.*=false;ffmpeg.*=false"
-        exec ${pkgs.telegram-desktop}/bin/Telegram "$@"
-        EOF
-        chmod +x $out/bin/telegram
-      '';
-in
 {
   options = {
     messengers.enable = lib.mkEnableOption "enables messengers module";
   };
 
   config = lib.mkIf config.messengers.enable {
-    environment.systemPackages = [
-      telegramWrapper
+    environment.systemPackages = with pkgs; [
+      telegram-desktop
     ];
   };
 }
