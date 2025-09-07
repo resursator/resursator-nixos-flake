f609a920ecc08644e5e9e99db2c242bddd2a77de
resursator <resursator@gmail.com>
2025-09-08 00:36:11 +0300

    telegram wrapper to remove spam in jctl

--- modules/programs/messengers.nix
+++ modules/programs/messengers.nix
@@ -4,14 +4,30 @@
   pkgs,
   ...
 }:
+let
+  telegramWrapper =
+    pkgs.runCommand "telegram-desktop-wrapper"
+      {
+        buildInputs = [ pkgs.telegram-desktop ];
+      }
+      ''
+        mkdir -p $out/bin
+        cat > $out/bin/telegram <<'EOF'
+        #!${pkgs.stdenv.shell}
+        export QT_LOGGING_RULES="qt.gui.imageio.*=false;ffmpeg.*=false"
+        exec ${pkgs.telegram-desktop}/bin/Telegram "$@"
+        EOF
+        chmod +x $out/bin/telegram
+      '';
+in
 {
   options = {
     messengers.enable = lib.mkEnableOption "enables messengers module";
   };
 
   config = lib.mkIf config.messengers.enable {
-    environment.systemPackages = with pkgs; [
-      telegram-desktop
+    environment.systemPackages = [
+      telegramWrapper
     ];
   };
 }
