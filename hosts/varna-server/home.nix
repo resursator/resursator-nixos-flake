0ff2ba06158720825904797e0bdb876924efd027
resursator <resursator@gmail.com>
2025-09-12 20:47:29 +0300

    variables

--- hosts/varna-server/home.nix
+++ hosts/varna-server/home.nix
@@ -15,27 +15,18 @@
   home.packages = [
   ];
 
-  # Home Manager can also manage your environment variables through
-  # 'home.sessionVariables'. These will be explicitly sourced when using a
-  # shell provided by Home Manager. If you don't want to manage your shell
-  # through Home Manager then you have to manually source 'hm-session-vars.sh'
-  # located at either
-  #
-  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  /etc/profiles/per-user/resursator/etc/profile.d/hm-session-vars.sh
-  #
   home.sessionVariables = {
     FLAKE = "/home/${USERNAME}/nixosFlake";
     NH_FLAKE = "/home/${USERNAME}/nixosFlake";
   };
 
-  # Let Home Manager install and manage itself.
+  home.file.".profile.d/home-manager.sh".text = ''
+    if [ -f "$HOME/.nix-profile/etc/profile.d/hm-session-vars.sh" ]; then
+      . "$HOME/.nix-profile/etc/profile.d/hm-session-vars.sh"
+    elif [ -f "$HOME/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh" ]; then
+      . "$HOME/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh"
+    fi
+  '';
+
   programs.home-manager.enable = true;
 }
