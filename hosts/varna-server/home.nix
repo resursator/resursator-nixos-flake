d945d66e1a83e97195731978a7592bd8db7e29f0
resursator <resursator@gmail.com>
2025-09-12 21:13:49 +0300

    removed escaping

--- hosts/varna-server/home.nix
+++ hosts/varna-server/home.nix
@@ -51,7 +51,7 @@
       HISTSIZE=1000
       HISTFILESIZE=2000
       shopt -s checkwinsize
-      if [ -z "$\{debian_chroot:-}" ] && [ -r /etc/debian_chroot ]; then
+      if [ -z "${debian_chroot:-}" ] && [ -r /etc/debian_chroot ]; then
           debian_chroot=$(cat /etc/debian_chroot)
       fi
       case "$TERM" in
@@ -65,14 +65,14 @@
           fi
       fi
       if [ "$color_prompt" = yes ]; then
-          PS1='$\{debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '
+          PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '
       else
-          PS1='$\{debian_chroot:+($debian_chroot)}\u@\h:\w\$ '
+          PS1='${debian_chroot:+($debian_chroot)}\u@\h:\w\$ '
       fi
       unset color_prompt force_color_prompt
       case "$TERM" in
       xterm*|rxvt*)
-          PS1="\[\e]0;$\{debian_chroot:+($debian_chroot)}\u@\h: \w\a\]$PS1"
+          PS1="\[\e]0;${debian_chroot:+($debian_chroot)}\u@\h: \w\a\]$PS1"
           ;;
       esac
       if [ -x /usr/bin/dircolors ]; then
