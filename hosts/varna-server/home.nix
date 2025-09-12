6ad998148d8adbfe8ca6de225e588f51df7222ae
resursator <resursator@gmail.com>
2025-09-12 21:29:30 +0300

    again

--- hosts/varna-server/home.nix
+++ hosts/varna-server/home.nix
@@ -50,7 +50,7 @@
       HISTSIZE=1000
       HISTFILESIZE=2000
       shopt -s checkwinsize
-      if [ -z "$$debian_chroot" ] && [ -r /etc/debian_chroot ]; then
+      if [ -z "$debian_chroot" ] && [ -r /etc/debian_chroot ]; then
           debian_chroot=$(cat /etc/debian_chroot)
       fi
       case "$TERM" in
@@ -64,14 +64,14 @@
           fi
       fi
       if [ "$color_prompt" = yes ]; then
-          PS1='$${debian_chroot:+($$debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '
+          PS1='($debian_chroot)\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '
       else
-          PS1='$${debian_chroot:+($$debian_chroot)}\u@\h:\$FLAKE\$ '
+          PS1='($debian_chroot)\u@\h:\$FLAKE\$ '
       fi
       unset color_prompt force_color_prompt
       case "$TERM" in
       xterm*|rxvt*)
-          PS1="\[\e]0;$${debian_chroot:+($$debian_chroot)}\u@\h: \$FLAKE\a\]$PS1"
+          PS1="\[\e]0;($debian_chroot)\u@\h: \$FLAKE\a\]$PS1"
           ;;
       esac
       if [ -x /usr/bin/dircolors ]; then
