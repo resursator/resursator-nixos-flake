a8ad81640cc3a894ddf67f0bb8d2f164a6d3b5d7
resursator <resursator@gmail.com>
2025-09-20 13:07:35 +0300

    proof of concept, systemd service to combine sops secrets

--- modules/home-manager-modules/ssh-keys.nix
+++ modules/home-manager-modules/ssh-keys.nix
@@ -1,14 +1,28 @@
 {
   lib,
   config,
-  # USERNAME,
+  pkgs,
   HOSTNAME ? "unknown-host",
+  USERNAME,
   ...
 }:
 let
   secretsFile = ../../secrets/secrets.yaml;
-  # sshdCheckNixAuth = "^[[:space:]]*AuthorizedKeysFile[[:space:]]+\.ssh/authorized_keys[[:space:]]+\.ssh/authorized_keys_nix([[:space:]]|$)";
-  # sshdWarning = "AuthorizedKeysFile .ssh/authorized_keys .ssh/authorized_keys_nix";
+  uid =
+    let
+      uidScript = pkgs.writeScript "get-uid" ''
+        #!${pkgs.runtimeShell}
+        id -u
+      '';
+    in
+    pkgs.lib.strings.toIntBase10 (
+      builtins.readFile (
+        pkgs.runCommand "get-uid-result" { } ''
+          ${uidScript} >$out
+        ''
+      )
+    );
+
 in
 {
   options.ssh-keys.enable = lib.mkOption {
@@ -27,23 +41,45 @@ in
       defaultSopsFile = secretsFile;
       secrets.ssh_varna-server_pubkey_unencrypted = {
       };
-      age.keyFile = "${config.home.homeDirectory}/.config/sops/age/keys.txt";
+      defaultSymlinkPath = "/run/user/${builtins.toString uid}/secrets";
+      defaultSecretsMountPoint = "/run/user/${builtins.toString uid}/secrets.d";
+      age.keyFile = "/run/user/${builtins.toString uid}/sops-age.txt";
     };
 
-    home.file.".ssh/authorized_keys_nix".text =
-      builtins.readFile config.sops.secrets.ssh_varna-server_pubkey_unencrypted.path;
+    home.file.".secretSopsPaths/pubkeys/ssh_varna-server_pubkey_unencrypted".text =
+      config.sops.secrets.ssh_varna-server_pubkey_unencrypted.path;
 
-    # home.activation.check-sshd = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-    #   files="/etc/ssh/sshd_config"
-    #   if [ -d /etc/ssh/sshd_config.d ]; then
-    #     for f in /etc/ssh/sshd_config.d/*; do [ -f "$f" ] && files="$files $f"; done
-    #   fi
+    systemd.user.services."merge-ssh-keys" = {
+      Unit = {
+        Description = "Merge sops-nix ssh key secrets into authorized_keys_nix";
+        After = [ "sops-nix.service" ];
+      };
 
-    #   if ! grep -Eq '${sshdCheckNixAuth}' $files 2>/dev/null; then
-    #     echo "⚠️ SSH WARNING: /etc/ssh/sshd_config не содержит строку:"
-    #     echo "  ${sshdWarning}"
-    #     echo "Добавь её вручную (или в /etc/ssh/sshd_config.d/10-nix-keys.conf)"
-    #   fi
-    # '';
+      Service = {
+        Type = "oneshot";
+        ExecStart = pkgs.writeShellScript "merge-ssh-keys" ''
+          set -euo pipefail
+
+          OUT="/home/${USERNAME}/.ssh/authorized_keys_nix"
+          : >"$OUT"
+
+          for f in "/home/${USERNAME}/.secretSopsPaths/pubkeys/"*; do
+            [ -f "$f" ] || continue
+            target=$(cat "$f")
+            if [ -f "$target" ]; then
+              cat "$target" >>"$OUT"
+              echo "" >>"$OUT"
+            else
+              echo "⚠ secret target $target not found" >&2
+            fi
+          done
+        '';
+      };
+
+      Install = {
+        WantedBy = [ "default.target" ];
+      };
+    };
+
   };
 }
