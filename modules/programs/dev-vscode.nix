843d51744e0dd7042e3c91d0a0ef0eea4f13b104
resursator <resursator@gmail.com>
2025-10-01 00:48:25 +0300

    Switch to vscode with extensions and update dev environment

--- modules/programs/dev-vscode.nix
+++ modules/programs/dev-vscode.nix
@@ -10,9 +10,15 @@
   };
 
   config = lib.mkIf config.dev-vscode.enable {
-    environment.systemPackages = [
-      pkgs.github-desktop
-      pkgs.vscode
+    environment.systemPackages = with pkgs; [
+      github-desktop
+      (pkgs.vscode-with-extensions.override {
+        vscode = pkgs.vscodium;
+        vscodeExtensions = with pkgs.vscode-extensions; [
+          continue.continue
+          jnoortheen.nix-ide
+        ];
+      })
     ];
     programs.direnv.enable = true;
   };
