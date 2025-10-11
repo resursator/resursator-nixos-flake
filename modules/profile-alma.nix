62ead274afeeee41832133d6e2181f559ec2b534
resursator <resursator@gmail.com>
2025-10-11 21:12:09 +0300

    Update profile-alma.nix

--- modules/profile-alma.nix
+++ modules/profile-alma.nix
@@ -20,10 +20,14 @@
 
   imports = [
     ./modulebundle.nix
+    inputs.sops-nix.nixosModules.sops
     inputs.home-manager.nixosModules.home-manager
     {
       home-manager.useGlobalPkgs = true;
       home-manager.useUserPackages = true;
+      home-manager.sharedModules = [
+        inputs.sops-nix.homeManagerModules.sops
+      ];
     }
   ];
 
