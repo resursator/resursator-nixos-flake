99f9ea811d2ab447e8d32ba30189bb74fbc282b3
resursator <resursator@gmail.com>
2025-10-11 20:24:10 +0300

    fix sops dep

--- modules/profile-alma.nix
+++ modules/profile-alma.nix
@@ -62,6 +62,7 @@
   openrgb.enable = lib.mkDefault false;
   plymouth.enable = lib.mkDefault false;
   rustdesk.enable = lib.mkDefault false;
+  sops.enable = lib.mkDefault false;
   ssh.enable = lib.mkDefault true;
   yggdrasil.enable = lib.mkDefault false;
   zerotier.enable = lib.mkDefault false;
