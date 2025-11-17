64da938139b275e4ef7bed0b00fd54a9e35a1956
resursator <resursator@gmail.com>
2025-11-18 02:28:49 +0300

    helm & docker ip range

--- modules/services/docker.nix
+++ modules/services/docker.nix
@@ -10,6 +10,9 @@
 
   config = lib.mkIf config.docker.enable {
     virtualisation.docker.enable = true;
+    virtualisation.docker.extraOptions = ''
+      --bip=172.31.0.1/16
+    '';
     users.users.resursator.extraGroups = [ "docker" ];
   };
 }
--- modules/services/k8t-kind.nix
+++ modules/services/k8t-kind.nix
@@ -11,6 +11,7 @@
 
   config = lib.mkIf config.k8t-kind.enable {
     environment.systemPackages = with pkgs; [
+      kubernetes-helm
       kind
       kubectl
       kubernetes-helm
