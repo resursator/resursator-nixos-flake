cbfe409721f44816180f407e9eac28667bddc909
resursator <resursator@gmail.com>
2026-01-08 18:31:23 +0300

    docker mirrors

--- modules/services/docker.nix
+++ modules/services/docker.nix
@@ -13,6 +13,13 @@
     virtualisation.docker.extraOptions = ''
       --bip=172.31.0.1/16
     '';
+    virtualisation.docker.daemon.settings = {
+      registry-mirrors = [
+        "https://mirror.gcr.io"
+        "https://dockerproxy.com"
+        "https://docker.mirrors.sjtug.sjtu.edu.cn"
+      ];
+    };
     users.users.resursator.extraGroups = [ "docker" ];
   };
 }
