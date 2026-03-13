75329050cdedf8e78bdebafe5424349819318bb7
resursator <resursator@gmail.com>
2026-03-13 22:38:47 +0300

    updated vesktop icons

--- modules/programs/vesktopCustom.nix
+++ modules/programs/vesktopCustom.nix
@@ -5,23 +5,26 @@
   ...
 }:
 let
-  customVesktopIcon = pkgs.runCommand "custom-vesktop-icon" {} ''
+  customVesktopIcon = pkgs.runCommand "custom-vesktop-icon" { } ''
     mkdir -p $out
     cp ${../../resources/discord/discord.icns} $out/icon.icns
     cp ${../../resources/discord/discord.ico} $out/icon.ico
+    cp ${../../resources/discord/discord.svg} $out/icon.svg
     cp ${../../resources/discord/discord-tray-icon.png} $out/tray-icon.png
-    cp ${../../resources/discord/discord-loading.gif} $out/loading.gif
+    cp ${../../resources/discord/discord-tray-icon-unread.png} $out/tray-icon-unread.png
+    cp ${../../resources/discord/discord-loading.webp} $out/loading.webp
   '';
 
   vesktopCustom = pkgs.vesktop.overrideAttrs (oldAttrs: {
-    # electron = pkgs.electron_33;
     preBuild = oldAttrs.preBuild or "" + ''
       echo "Replacing icon, icns and loading animation"
       cp ${customVesktopIcon}/icon.icns build/icon.icns
-      cp ${customVesktopIcon}/icon.ico static/icon.ico
-      cp ${customVesktopIcon}/tray-icon.png build/icon.png
-      cp ${customVesktopIcon}/tray-icon.png static/icon.png
-      cp ${customVesktopIcon}/loading.gif static/shiggy.gif
+      cp ${customVesktopIcon}/icon.svg build/icon.svg
+      cp ${customVesktopIcon}/icon.ico build/icon.ico
+      cp ${customVesktopIcon}/loading.webp static/splash.webp
+      cp ${customVesktopIcon}/tray-icon.png static/tray.png
+      cp ${customVesktopIcon}/tray-icon.png static/tray/tray.png
+      cp ${customVesktopIcon}/tray-icon-unread.png static/tray/trayUnread.png
     '';
   });
 in
